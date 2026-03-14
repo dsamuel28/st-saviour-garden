872b9692f79f8bf86c11ed48a0f82a499636bdd7
dsamuel <dsamuel28@stsaviour.org>
2026-03-13 21:29:42 -0400

    lab 5

--- garden.py
+++ garden.py
@@ -3,4 +3,54 @@
 if __name__ == '__main__':
 
     # print without a line break using this function
-    print('🌷', end='')
+    # print('🌱', end='')
+    # print('🌺', end='')
+
+    print('solid')  
+    for i in range(11):
+        for j in range(11):
+            print ('🌱', end='')
+        print('')
+
+    print('horizontal') 
+    for i in range(11):
+        for j in range(11):
+            if i % 2 == 0: 
+                print('🌱', end='')  
+            else: 
+                print('🌺', end='')
+        print('')
+
+    print('vertical') 
+    for i in range(11):
+        for j in range(11): 
+            if j % 2 == 0: 
+                print('🌱', end= '')
+            else:
+                print('🌺', end='')
+        print('')
+    
+    print('diagional')
+    for i in range(11):
+        for j in range(11):
+            if j % 2 == 0 and i % 2 == 0:
+                print('🌱', end= '')
+            elif j % 2 == 1 and i % 2 == 1:
+                print('🌱', end ='')
+            else: 
+                print('🌺', end= '')
+        print('')
+    
+    print ('cross')
+    for row in range(11):
+        for col in range(11):
+            if row == col or row + col ==10:
+                print('🌱', end= '')
+            else:
+                print('🌺', end= '')
+            print()
+            
+        
+
+
+                
